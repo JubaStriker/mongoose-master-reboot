f0b690e79c4e6fd19e6fba253080d9489f827cf8
JubaStriker <jubastriker@gmail.com>
2023-12-21 00:44:16 +0600

    Public used in constructor

--- src/app/builder/QueryBuilder.ts
+++ src/app/builder/QueryBuilder.ts
@@ -1,10 +1,10 @@
 import { FilterQuery, Query } from 'mongoose';
 
 class QueryBuilder<T> {
-  public modelQuery: Query<T[], T>;
-  public query: Record<string, unknown>;
+  // public modelQuery: Query<T[], T>;
+  // public query: Record<string, unknown>;
 
-  constructor(modelQuery: Query<T[], T>, query: Record<string, unknown>) {
+  constructor(public modelQuery: Query<T[], T>, public query: Record<string, unknown>) {
     this.modelQuery = modelQuery;
     this.query = query;
   }
